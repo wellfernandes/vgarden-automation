746d048c87f779959f6d34f8055c7a33a7437d73
Welliton Fernandes Leal <wellfernandes@me.com>
2022-07-15 21:40:04 -0300

    lcd visor configured

--- arduino/vgarden-automation-arduino/vgarden-automation-arduino.ino
+++ arduino/vgarden-automation-arduino/vgarden-automation-arduino.ino
@@ -1,9 +1,21 @@
-void setup() {
-  
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+const int pinSensor = A0;  //pin used by the humidity sensor
+int readedValue;           //soil moisture percentage
+
+// Initialize the display at the address 0x3F
+// the other parameters are necessary for the module to connect with the LCD
+// parameter: POSITIVE Backlight ON and NEGATIVE Backlight OFF
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, NEGATIVE);
+
+void setup() {
+  Serial.begin(9600);                              //serial init
+  Serial.println("Reading the soil moisture...");  //print the text on monitor serial
+  delay(2000);                                     //2 seconds interval
 }
 
 void loop() {
- 
-
+  
 }
