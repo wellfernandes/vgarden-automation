66e0fb6bdfc0c63f38a82092dc52d1ed1e3e2173
Welliton Fernandes Leal <wellfernandes@me.com>
2022-07-20 14:37:33 -0300

    adjusted conditions and removed irrigation status variable

--- arduino/vgarden-automation-arduino/vgarden-automation-arduino.ino
+++ arduino/vgarden-automation-arduino/vgarden-automation-arduino.ino
@@ -16,11 +16,8 @@ const int drySoil = 60;
 const int dampSoil = 80;
 const int soakedSoil = 95;
 
-// irrigation state on = 1 or off = 0
-bool irrigationState = 0;
-
 //soil moisture percentage
-int readValue;  
+int readValue = 0;
 
 // Initialize the display at the address 0x3F
 // the other parameters are necessary for the module to connect with the LCD
@@ -35,8 +32,8 @@ void setup() {
   pinMode(pinLedOff, OUTPUT);
   digitalWrite(pinLedOn, LOW);
   digitalWrite(pinLedOff, HIGH);
-  
-   // initialize the display with 16 columns x 2 rows or 20,4 if it's the display 20x4
+
+  // initialize the display with 16 columns x 2 rows or 20,4 if it's the display 20x4
   displayLcd.begin(16, 2);
   displayLcd.setBacklightPin(3, POSITIVE);  // NEGATIVE is the black letter and dark background
 
@@ -46,15 +43,14 @@ void setup() {
 
   displayLcd.home();
   displayLcd.setCursor(0, 0);
-  displayLcd.println("     Vgarden    "); 
+  displayLcd.println("     Vgarden    ");
   displayLcd.setCursor(0, 1);
-  displayLcd.println("   iniciando...  "); // intro text - getting started
+  displayLcd.println("   iniciando...  ");  // intro text - getting started
   delay(10000);
 
-  displayLcd.clear();
   displayLcd.home();
   displayLcd.setCursor(0, 0);
-  displayLcd.println("recebendo dados "); 
+  displayLcd.println("recebendo dados ");
   displayLcd.setCursor(0, 1);
   displayLcd.println("   aguarde...   ");
   delay(5000);
@@ -64,18 +60,16 @@ void setup() {
 void loop() {
   Wire.begin();
   readValue = analogRead(pinSensor);
-
   Serial.println(readValue);
-
+  
   // sensor - soil value, maximum, divisible and positive
-  readValue = map(readValue, 290, 1022, 100, 0);  
+  readValue = map(readValue, 290, 1022, 100, 0);
   // Serial.print("Umidade do solo: "); // to print in the terminal
   // Serial.print(readValue);
   // Serial.println("%  ");
   
-  displayLcd.home();
   displayLcd.setCursor(0, 0);
-  displayLcd.print("Humidade:    ");
+  displayLcd.print("Humidade:          ");
   displayLcd.setCursor(10, 0);
   displayLcd.print(readValue);
   displayLcd.setCursor(14, 0);
@@ -84,41 +78,58 @@ void loop() {
   if (readValue <= drySoil) {
     displayLcd.setCursor(0, 1);
     displayLcd.print("Solo muito seco   ");  // very dry soil
-    if (irrigationState == 0) {
-      irrigationState = 1;
-      digitalWrite(pinRelay, HIGH);
+    delay(10000);
+
+    // check value to irrigate
+    readValue = analogRead(pinSensor);
+    readValue = map(readValue, 290, 1022, 100, 0);
+
+    displayLcd.home();
+    displayLcd.clear();
+    displayLcd.setCursor(0, 0);
+    displayLcd.print("Humidade:           ");
+    displayLcd.setCursor(10, 0);
+    displayLcd.print(readValue);
+    displayLcd.setCursor(14, 0);
+    displayLcd.print("%  ");
+    displayLcd.setCursor(0, 1);
+    displayLcd.print("   Confirmado       ");
+    delay(1000);
+    
+    if (readValue <= drySoil) {
       digitalWrite(pinLedOn, HIGH);
       digitalWrite(pinLedOff, LOW);
+      displayLcd.setCursor(0, 0);
+      displayLcd.print("   Irrigando       ");
+      displayLcd.setCursor(0, 1);
+      displayLcd.print("   aguarde...      ");
+      digitalWrite(pinRelay, HIGH);
+      delay(120000);  // 2 min of irrigation
     }
-    digitalWrite(pinRelay, HIGH);
   } else if (readValue > drySoil && readValue < dampSoil) {
-    displayLcd.setCursor(0, 1);
-    displayLcd.print("Solo pouco seco   ");  // dry soil
-  } else if (readValue >= dampSoil && readValue < soakedSoil) {
-    if (irrigationState == 1) {
-      irrigationState = 0;
       digitalWrite(pinRelay, LOW);
       digitalWrite(pinLedOn, LOW);
       digitalWrite(pinLedOff, HIGH);
+      displayLcd.setCursor(0, 1);
+      displayLcd.print("Solo pouco seco   ");  // dry soil
+    } else if (readValue >= dampSoil && readValue < soakedSoil) {
+      digitalWrite(pinRelay, LOW);
+      digitalWrite(pinLedOn, LOW);
+      digitalWrite(pinLedOff, HIGH);
+      displayLcd.setCursor(0, 1);
+      displayLcd.print("Umidade ideal     ");  // ideal soil
+    } else if (readValue >= soakedSoil) {
+      digitalWrite(pinRelay, LOW);
+      digitalWrite(pinLedOn, LOW);
+      digitalWrite(pinLedOff, HIGH);
+      displayLcd.setCursor(0, 1);
+      displayLcd.print("Solo encharcado   ");  // soggy soil
+    } else {
+      digitalWrite(pinLedOn, LOW);
+      digitalWrite(pinLedOff, LOW);
+      displayLcd.setCursor(0, 1);
+      displayLcd.print("Erro na leitura   ");  // sensor reading error
     }
-    displayLcd.setCursor(0, 1);
-    displayLcd.print("Umidade ideal  ");  // ideal soil
-    digitalWrite(pinLedOn, LOW);
-    digitalWrite(pinLedOff, HIGH);
-  } else if (readValue >= soakedSoil) {
-    irrigationState = 1;
-    displayLcd.setCursor(0, 1);
-    displayLcd.print("Solo encharcado   ");  // soggy soil
-    digitalWrite(pinLedOn, LOW);
-    digitalWrite(pinLedOff, HIGH);
-  } else {
-    displayLcd.setCursor(0, 1);
-    displayLcd.print("Erro na leitura   ");  // sensor reading error
-    digitalWrite(pinLedOn, LOW);
-    digitalWrite(pinLedOff, HIGH);
+    // 2 seconds interval
     delay(2000);
-    digitalWrite(pinLedOff, HIGH);
-  }
-  // 2 seconds interval
-  delay(2000);  
 }
